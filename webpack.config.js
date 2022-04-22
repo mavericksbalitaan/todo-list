a01af180cf78ffd22efc0e86148dd1fdcb5bd416
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-22 17:40:48 +0500

    Configure loaders in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
 };
