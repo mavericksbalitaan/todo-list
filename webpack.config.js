4dd5519a0d7b4262e27d737f3aa0466c65659445
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-22 17:50:39 +0500

    Configure webpack-dev-server in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -15,6 +16,9 @@ module.exports = {
         use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
+  },
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
