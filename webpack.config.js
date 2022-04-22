a76fe2172e5d57abaa7bd4b66762bb307c471af2
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-22 17:47:07 +0500

    Configure plugin in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -15,4 +16,9 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
 };
